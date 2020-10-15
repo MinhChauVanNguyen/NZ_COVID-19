2b13cc3b4524e790c25cff04ab3888575c2265bd
Connor Wallis <con@Connors-MBP.fritz.box>
2020-10-15 16:39:54 +1300

    new

--- function.R
+++ function.R
@@ -1,38 +1,45 @@
+fake_dt <- read_excel("Analyst_Lab_Data 2.xls")
+true_dt <- read_excel("Population_Data 2.xls")
+names(true_dt)[2] <- "SA22018_V1_00"
 
-x <- c(95, 86.5, 80.8, 80.4, 80.3, 78.4, 74.2, 73.5, 71, 69.2, 68.6, 65.5)
-y <- c(95, 102.9, 91.5, 102.5, 86.1, 70.1, 68.5, 83.1, 93.2, 57.6, 20, 126.4)
-z <- c(13.8, 14.7, 15.8, 12, 11.8, 16.6, 14.5, 10, 24.7, 10.4, 16, 35.3)
-name <- c("BE", "BE", "BE", "BE", "SE", "SE", "SE", "SE", "UK", "UK", "UK", "UK")
-country <- c("Belgium", "Belgium", "Belgium", "Belgium",
-             "Sweden", "Sweden", "Sweden", "Sweden",
-             "United Kingdom", "United Kingdom", "United Kingdom", "United Kingdom")
-ethnicity <- c("Maori", "Pacific", "Asian", "Other", "Maori", "Pacific", "Asian", "Other",
-               "Maori", "Pacific", "Asian", "Other")
+# excluding those that replied "Unknown" for Ethnicity (n = 1656 observations)
+full_dt <- merge(fake_dt, true_dt, by = c("SA22018_V1_00", "Ethnicity"))
 
+full_dt$TA2018_V1_00_NAME <- factor(full_dt$TA2018_V1_00_NAME)
 
-data <- data.frame(x, y, z, name, country, ethnicity)
+big_list <- list()
 
-highchart() %>%
-  hc_chart(type = "bubble", plotBorderWidth = 1) %>%
-  hc_add_series(data = list_parse(data[data$ethnicity == "Maori", ]), color = "#AFEEEE", name = "Maori") %>%
-  hc_add_series(data = list_parse(data[data$ethnicity == "Pacific", ]), color = "#ff83c1", name = "Pacific", visible = FALSE) %>%
-  hc_add_series(data = list_parse(data[data$ethnicity == "Asian", ]), color = "#7CFC00", name = "Asian", visible = FALSE) %>%
-  hc_add_series(data = list_parse(data[data$ethnicity == "Other", ]),  name = "Other", visible = FALSE) %>%
-  hc_xAxis(title = list(text = 'Daily fat intake'), labels = list(format = '{value} gr'), gridLineWith = 1) %>%
-  hc_yAxis(startOnTick = FALSE, endOnTick = FALSE, title = list(text = 'Daily sugar intake'),
-           labels = list(format = '{value} gr'), maxPadding = 0.2, gridLineWidth = 1) %>%
-  hc_plotOptions(
-    series = list(
-      dataLabels = list(
-        enabled = TRUE, format = '{point.name}'))) %>%
-  hc_tooltip(
-    useHTML = TRUE, 
-    headerFormat = '<table>',
-    pointFormat = "<tr><th colspan='2'><h3>{point.country}</h3></th></tr> 
-       <tr><th>Fat intake:</th><td>{point.x}g</td></tr> 
-       <tr><th>Sugar intake:</th><td>{point.y}g</td></tr> 
-       <tr><th>Obesity (adults):</th><td>{point.z}%</td></tr>",
-    footerFormat ='</table>',
-    followPointer = TRUE)
+test_rate <- function(highly_deprived_areas = TRUE){
+  for(i in 1:length(levels(dd$TA2018_V1_00_NAME))){
+    lvl <- levels(full_dt$TA2018_V1_00_NAME)[i]
+    data <- full_dt[full_dt$TA2018_V1_00_NAME == lvl,]
+    if(highly_deprived_areas == TRUE){
+      subset_data <- data[which(data$Number_of_Tests < data$Census_2019_Population_Estimate & data$SA2_average_NZDep2018 > 5),]
+    }else{
+      subset_data <- data[which(data$Number_of_Tests < data$Census_2019_Population_Estimate & data$SA2_average_NZDep2018 < 5),]
+    }
+    subset_data <- subset_data[,!(names(subset_data) %in% c("SA22018_V1_00", "SA2_average_NZDep2018_score"))]
+    big_list[[i]] <- subset_data
+  }
+  
+  data <- do.call(rbind.data.frame, big_list)
+  names(data) <- c("Ethnicity", "NZDep", "Area", "District", "Tests", "Population")
+  data <- data %>% mutate(Rate = Tests/Population)
+  data <- data[which(data$Rate < 0.20),] 
+  data$Rate <- percent(data$Rate, 1)
+  data$District <- factor(data$District)
+  
+  return(data)
+}
 
+highly_deprived <- test_rate()
+low_deprived <- test_rate(FALSE)
 
+# regions that both in low_deprived data and high_deprived data
+levels(highly_deprived$District)[which(levels(highly_deprived$District) %in% levels(low_deprived$District))]
+
+# regions that are in high_deprived data and not in low_deprived data
+setdiff(levels(highly_deprived$District), levels(low_deprived$District))
+
+# regions that are in low_deprived data and not in high_deprived data
+setdiff(levels(highly_deprived$District), levels(highly_deprived$District))
